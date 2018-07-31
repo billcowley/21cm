0ace8974859a13bf75a30946e3cfaa1108c202a3
pi <billcowley42@gmail.com>
2018-07-31 12:58:53 +0930

    add extra RTL checking

--- rtl_proc.py
+++ rtl_proc.py
@@ -10,7 +10,9 @@
 #    Requires RTL2832 to be plugged in and pyrtlsdr to be installed
 #
 #    Bill 2018 
-
+#
+#   add option for discarding samples ... "python rtl_proc.py -d"
+     
 from __future__ import division
 from __future__ import print_function
 from rtlsdr import *
@@ -27,7 +29,7 @@ def main():
     def test_callback(samples, rtlsdr_obj):
         print('  in callback')
         print('  signal mean:', sum(samples)/len(samples))
-
+    sdrFail=0
     sdr = RtlSdr()
 
     pfname = 'rtlpars.txt'
@@ -44,15 +46,22 @@ def main():
     print('')
     freqStored = 0
     
+    discardMode = 0
+    if len(sys.argv)>1:
+        if str(sys.argv[1])=="-d":
+            discardMode = 1;
+            print(" running in discard mode ")
+
     while (1): 
 
        for m in range(1,Nband+1):
            
            dfname = 'psd'+'%d'%m +'.txt'
-           while (os.path.isfile(dfname)==1):
-               #print(' wait for file ' + dfname + ' to disappear');
-               sys.stdout.write('w');   sys.stdout.flush()
-               time.sleep(4)
+           if discardMode==0: 
+              while (os.path.isfile(dfname)==1):
+                 #print(' wait for file ' + dfname + ' to disappear');
+                 sys.stdout.write('w');   sys.stdout.flush()
+                 time.sleep(4)
            try:        # reopen to allow changes on the fly 
                pars = ''
                with open(pfname,'r') as pfile:
@@ -63,24 +72,36 @@ def main():
                exit()
 
            if len(pars)>1:
-#               print('collect data from chan'+str(m))
+               print('collect data from chan'+str(m))
 #               print(sdr)
-               if (m==1): sdr.fc = Fc1
-               if (m==2): sdr.fc = Fc2
-               sdr.rs = Fs
-               sdr.gain = Gain
-               print('Fc %0.3f MHz '  %(sdr.fc/1e6) + ' Fs %0.3f MHz'
+               try: 
+                  if (m==1): sdr.fc = Fc1
+                  if (m==2): sdr.fc = Fc2
+                  sdr.rs = Fs
+                  sdr.gain = Gain
+                  print('Fc %0.3f MHz '  %(sdr.fc/1e6) + ' Fs %0.3f MHz'
                      %(sdr.rs/1e6) + ' Gain %0.1f dB' %Gain)
-               print('Reading samples...')
-               samples = sdr.read_samples(Nsam)
-# demean has little effect          samples = samples - np.mean(samples)
+                  print('Reading samples...')
+                  samples = sdr.read_samples(Nsam)
+               except:
+                  print("sdr failed")
+                  sdrFail=1
+                  time.sleep(1)
+                  sdr.close()
+                  time.sleep(1)
+                  break
+                        
+               # demean has little effect          samples = samples - np.mean(samples)
                # returns floating point complex samples 
                print(' std %3.1f bits' % np.std(samples*128))
 
                # Welch PSD with default Hanning, no overlap
                f, Pxx = signal.welch(samples, fs=Fs, detrend = 'constant', nperseg=Nw)
-               Pxx[0] = (Pxx[1]+Pxx[-1])/2.0   # replace DC value 
-#               print('PSD calculated')
+               Pxx[0] = (Pxx[3]+Pxx[2]+Pxx[-2]+Pxx[-3])/4.0   # replace DC value
+               Pxx[1] = Pxx[0];
+               Pxx[-1]= Pxx[0];  
+
+               print('PSD calculated')
                if (freqStored==0):
                    np.savetxt('freqs.txt', f, fmt='%12.1f')
                    freqStored=1
@@ -93,6 +114,10 @@ def main():
                print('bad params, skipping ??')
                
            time.sleep(0.1)
+       if sdrFail==1:
+           time.sleep(1)
+           sdr = RtlSdr()
+           sdrFail = 0 # hopefully reopen RTL dongle 
            
     print('Done\n')
     sdr.close()
