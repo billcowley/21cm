d67f2c4dd31bb48e52b5f1157ece958977a180af
Bill Cowley <billcowley42@gmail.com>
2021-07-19 12:36:19 +0930

    Add files via upload
    This (experimental) version of the log file viewer includes an option to avoid variations in the feed power level.

--- readh21log.m
+++ readh21log.m
@@ -4,6 +4,8 @@
 % the mesh plot can be rotated using the figure viewing tools
 % this version tries to handle multiple sets of data in the one log file!
 %
+% set fname before running (or spect.log is used)
+%
 % set Npt number of spectra per plot;
 % or set numb of hours to plots ('samePH')
 % add 2nd plot showing results in 1D
@@ -13,18 +15,23 @@
 
 if exist('deb')~=1,    deb =0,    end
 if exist('Npt')~=1,    Npt=2000,   end;  % how many records per plot?
-if exist('plt')~=1,    plt =1,    end;  % one plot or 2 plots?
-if exist('Fmin')~=1,   Fmin=1420.1; end  % limit the plot range
-if exist('Fmax')~=1,   Fmax=1420.8; end
-if exist('samePH')~=1, samePH =6, end % plot period in hrs
+if exist('plt')~=1,    plt =3,    end;  % one plot or 2 plots?
+if exist('Fmin')~=1,   Fmin=1419.9; end  % limit the plot range
+if exist('Fmax')~=1,   Fmax=1420.9; end
+if exist('samePH')~=1, samePH =24, end % plot period in hrs
 if exist('Iskip')~=1,  Iskip = 2; end % initial records to skip in plot
 if exist('psdVid')~=1, psdVid =0; end % set to 1 for video of raw PSDs
 if exist('Nvid')~=1,   Nvid = 1500; end;  % frames in video
 if exist('poly7')~=1,  poly7 =0;    end   % used for poly fitting method
-                                          % TBC  see code below at EOF
-if exist('chkGIF')~=1, chkGIF=0;    end   % use for making animated gif 
+if exist('PFIX')~=1,   PFIX =0;     end   % avoid feed power variations; July 2021
+% TBC  see code below at EOF
+if exist('chkGIF')~=1, chkGIF=0;    end   % use for making animated gif
+if exist('autorun')~=1, autorun=0;  end   % don't ask 
 
 samesecs = samePH * 3600;      % conv to secs
+if chkGIF>0,  makeGIF.active=0; end  
+
+clear pmax*
 
 % if exist('noslope')~=1,  noslope = 0; slop=[]; end % controls PSD slope adj
 % if noslope
@@ -49,7 +56,7 @@ if psdVid==1,
         psdVid=0;
     else
         jvid = 0;
-        vidObj = VideoWriter('h21psd.avi');
+        vidObj = VideoWriter('h21psd.mp4');
         disp('created video object ');
         open(vidObj);
     end
@@ -57,6 +64,7 @@ end
 
 while (1)
     n =1; clear pows; clear secs pmax;  clear pks*; sameperiod =1;
+    clear c4s;
     
     while (~feof(fid))&&(n<Npt) % while not EOF
         
@@ -96,8 +104,24 @@ while (1)
                 disp(['starting at ' startup]);
             end;
             inx=findstr(line1, 'Pmx');
-            if (inx>0)
-                pmax(n) = str2num(line1(inx+3:end));
+            if length(inx)>1    % new format of log file with two Pmx vals
+                pmax2(n) = str2num(line1(inx(2)+4:end));
+                pmax(n) = str2num(line1(inx(1)+4:inx(2)-1));
+            else
+                if (inx>0)
+                    pmax(n) = str2num(line1(inx+3:end));
+                end
+            end
+            coordstr= line1(17:inx-1);   % get the 4 co-ordinates
+            coordstr = strrep(coordstr,',',' ');
+            c4 = sscanf(coordstr, '%f');
+            if length(c4)==4,
+                c4s(n, :) = c4';               % add into matrix
+            else
+                disp(['bad value? - line is ', line1])
+                if autorun==0,  error('no coordinate string ?? ');
+                else, c4s(n, :) = c4s(n-1, :); 
+                end 
             end
             
         elseif line1(1:4)=='freq'
@@ -110,7 +134,12 @@ while (1)
             pow2 = sscanf(line2, ' %f', Nw);
             if ((length(pow1)==Nw) & (length(pow2)==Nw))
                 if poly7==0
-                    psd_diff = pow1'-pow2';    % store pow in array
+	            if PFIX
+	               pmean = mean(pow1) - mean(pow2);
+		    else,  pmean=0;
+                    end 
+                  
+                    psd_diff = pow1'-pow2'  -pmean;    % store pow in array
                 else
                     pp1 = pow1' -polyval(pol, frq-1420);
                     pp2 = pow2' - polyval(pol2, frq-1420);
@@ -132,9 +161,9 @@ while (1)
                         plot(pow1); hold on;
                         plot(pow2, 'k');
                         title(timenow);
-                        ax = axis; ax(3) = ax(4)-5; % 5 dB range
+                        ax = axis; ax(3) = ax(4)-5; % 3 dB range
                         axis(ax);
-                        pause(0.02);
+                        pause(0.2);
                         
                         if psdVid==1
                             jvid = jvid + 1;
@@ -169,9 +198,9 @@ while (1)
                     end
                     if chkGIF>0,
                         h_now = str2num(timenow(end-5:end-4)); % get hours now
-                        if ((makeGIF.start>=h_now) & (makeGIF.stop>h_now))
+                        %if ((makeGIF.start>=h_now) & (makeGIF.stop>h_now))
                             if makeGIF.active==0,
-                                makeGIF.active =1;  
+                                makeGIF.active =1;
                                 if chkGIF==201, gif('h21_anim.gif', 'frame', f201);deb=2; end
                                 if chkGIF==202, gif('h21_anim.gif', 'frame', f202);deb=4; end
                                 if chkGIF==203, gif('h21_anim.gif', 'frame', f203);deb=5; end
@@ -180,24 +209,28 @@ while (1)
                                 if chkGIF==202, gif('frame', f202); end
                                 if chkGIF==203, gif('frame', f203); end
                             end
-                        else
-                            if makeGIF.active ==1,
-                                makeGIF.active = 0; 
-                                %gif('clear', 'frame', f201); 
-                                deb = 1;
-                            end
-                        end
-                    end
+%                         else
+%                             if makeGIF.active ==1,
+%                                 makeGIF.active = 0;
+%                                 %gif('clear', 'frame', f201);
+%                                 deb = 1;
+%                             end
+                     end
+                    %end
                     
                 end
                 n=n+1;
                 if (sameperiod==0) break; end;
             else
                 disp('pow1 and pow2 are different lengths??')
+                length(pow1)
+                length(pow2)
             end
         end
         
     end
+    
+    
     
     
     if length(secs)>1
@@ -212,6 +245,28 @@ while (1)
             sec0 = secs(i);
             secn(i) = secs(i)+days*24*3600;
         end
+        
+        % plot the Gal trajectory
+        inn =  c4s(:,4)>180;
+        c4s(inn,4)= c4s(inn,4)-360;
+        figure(105),   plot(c4s(:,4), c4s(:,3))
+        xlabel('Gal Long ');  ylabel('Gal Lat');
+        
+        inbad = find(c4s(:,1)>125);    % check for large elevation bug
+        if length(inbad)>0
+            disp(' maybe bad elevations ?? ')
+            c4s(inbad,:)
+            if autorun==0, 
+                rep = input('delete these values? (y,n) ','s')
+            else rep = 'y'; 
+            end
+            if rep=='y'
+                c4s(inbad,:)=[]; pows(inbad, :) = [];  secs(inbad)=[];
+                pks_max(inbad) =[];  pks_frq(inbad) =[];  pmax(inbad)=[];
+            end
+        end
+        
+        
         
         %         nxti = secs<10000;   % fix after midnight times
         %         secs(nxti) = secs(nxti) + 24*3600;
@@ -239,33 +294,76 @@ while (1)
         ylabel(ylab)
         
         if exist('pmax')==1
-            figure(105);
+            figure(104);    clf;  
             plot(secs/3600, 10*log10(pmax));
             xlabel('Hours'); ylabel('Max Power')
+            if exist('pmax2')==1
+                figure(104);   hold on;  
+                plot(secs/3600, 10*log10(pmax2), 'r');
+                legend('feed', 'term')
+            end
         end
         
-        if plt>1
+        
+        
+        
+        if plt==2
             % show intensity and colour in 1D
-            disp('replot in 1D')
+            disp('replot in 2D Galactic -- ')
             figure(101)
             %cmap=colormap('default');
             cmap = colormap;
             Am= min(pks_max); AM=max(pks_max);
             %Fm= min(pks_frq); FM=max(pks_frq);
-            Fm = 1420.2; FM = 1420.8;
-            for n=1:length(secn)
-                h=plot(secn(n),0, 'x'); hold on;
+            %Fm = 1420.2; FM = 1420.8;
+            Fm = Fmin;   FM = Fmax;
+            Am = 0.1,   AM = 1.5
+            for n=1:length(c4s)
+                %h=plot(secn(n),0, 'x'); hold on;
+                h = plot(c4s(n,4), c4s(n,3), 'o'); hold on;
                 nw = 1+floor((pks_max(n)-Am)/(AM-Am)*15);
                 nc =floor((pks_frq(n)-Fm)/(FM-Fm)*63);
                 if nc<1, nc=1; end;  if nc>63, nc=63; end
                 set(h,'color', cmap(nc,:), 'markersize',nw);
-                title('1D peak plot: size=level, colour=freq')
-                xlabel(ylab);
+                title('peak plot: size=level, colour=freq')
+                xlabel('Gal Long (l) ');  ylabel('Gal Lat (b) ');
+                
             end
         end
-        disp('pausing until key pressed');
-        if (psdVid==0)  pause;
-        else  pause(3);  end
+        
+        if plt==3
+            garray = zeros(180,360,3);    % +180 to long >> index etc
+            sizeG = size(garray);  
+            % 3rd dimension is RGB value
+            Rind = frq2>1420.0  & frq2<1420.3;
+            Gind = frq2>1420.3  & frq2<1420.5;
+            Bind = frq2>1420.5  & frq2<1420.8;
+            
+            for nn = 1:length(c4s)
+                Rtot = Rind*pows(nn,:)'; Gtot = Gind*pows(nn,:)';Btot = Bind*pows(nn,:)';
+                latdim = ceil(c4s(nn,3))+90; if latdim>180, error('G lat ind too large'), end
+                londim = ceil(c4s(nn,4))+180; if londim>360, error('G long ind too large'), end
+                garray(latdim,londim, :)=[Rtot Gtot Btot];
+            end
+            figure(103);
+            if size(garray)~=sizeG,  error('garray size ??'), end 
+            if exist('garray_sum')==0
+                garray_sum = garray;
+            else
+                garray_sum = garray_sum+garray;
+            end
+            for k=1:3
+                garray(:,:,k) = conv2(garray(:,:,k), ones(4,4), 'same');
+            end
+            Isum = sum(garray,3);   % sum the RGBs
+            Imax = max(Isum(:))
+            image(-180:179, 89:-1:-90, 2*garray/Imax);    % 2 for brighter :)
+        end
+        
+        if feof(fid)==0, disp('pausing until key pressed');
+            if (psdVid==0)  pause;
+            else  pause(3);  end
+        end
     end
     if feof(fid), disp('all done'); break, end
     
@@ -275,8 +373,8 @@ fclose(fid);
 
 
 %{
-% commands used to model no-signal psd for 'pow1'  at 1420MHz 
-% saved with 
+% commands used to model no-signal psd for 'pow1'  at 1420MHz
+% saved with
 % --- save('poly_h21_512.mat', 'pol', 'pol2', 'fit', '-v7');
 
 rng = 101:400;
@@ -285,8 +383,8 @@ figure;   plot(frq(rng), pow1(rng))
 fpp = frq(rng);    ppp = pow1(rng);
 
 
-% following not needed -- DC offset has been removed 
-%ppp(158)=-.1 
+% following not needed -- DC offset has been removed
+%ppp(158)=-.1
 %ppp(156)=-.05
 %ppp(157)=-.09
 
